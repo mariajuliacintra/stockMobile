05512656af439081c9b0fd9589e97df6675fc33c
MariaFernandaCintra <mariafernandaccintra@gmail.com>
2025-03-19 16:22:55 -0300

    Começo da tela de perfil

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./screens/Login";
 import Principal from "./screens/Principal";
 import Home from "./screens/Home";
 import Perfil from "./screens/Perfil"
+import MinhasReservas from "./screens/MinhasReservas";
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,7 @@ export default function App() {
         <Stack.Screen name="Principal" component={Principal} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Perfil" component={Perfil} />
+        <Stack.Screen name="MinhasReservas" component={MinhasReservas}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Perfil.js
+++ src/screens/Perfil.js
@@ -11,11 +11,44 @@ import {
 
 export default function Perfil({ navigation }) {
   return (
-    <View>
+    <View style={styles.Header}>
+      <TouchableOpacity
+        style={styles.buttonToMinhasReservas}
+        onPress={() => navigation.navigate("MinhasReservas")}>
+          <Text style={styles.textButtonToMinhasReservas}>Minhas Reservas</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Header:{
+    backgroundColor: "rgba(177, 16, 16, 1)",
+    height: 70,
+    width: 500,
+    marginTop: 3,
+    borderBottomColor: "white",
+    borderBottomWidth: 3,
+    flexDirection: "row",
+  },
+  buttonToMinhasReservas:{
+    backgroundColor: "rgb(250, 24, 24)",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    alignItems: "center",
+    marginRight: 10,
+    borderColor: "white",
+    borderWidth: 2,
+    marginLeft: 280,
+    marginTop: 10,
+    height: 20,
+  },
+  textButtonToMinhasReservas:{
+    fontSize: 14,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
 
 
