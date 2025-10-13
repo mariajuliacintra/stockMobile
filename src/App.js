d1da692be41debd92374f3fae0c941daf4873a1c
MariaFernandaCintra <mariafernandaccintra@gmail.com>
2025-10-13 07:51:51 -0300

    Atualização de pastas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Principal from "./screens/Principal";
 import NoUsersScreen from "./screens/NoUsersScreen";
 import Home from "./screens/Home";
 import Perfil from "./screens/Perfil";
-import Itens from "./screens/Itens"
 import { StatusBar } from "react-native";
 
 const Stack = createStackNavigator();
@@ -17,7 +16,6 @@ export default function App() {
         <Stack.Screen name="Principal" component={Principal} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Perfil" component={Perfil} />
-        <Stack.Screen name="Itens" component={Itens}/>
         <Stack.Screen name="NoUsers" component={NoUsersScreen} />
       </Stack.Navigator>
     </NavigationContainer>
